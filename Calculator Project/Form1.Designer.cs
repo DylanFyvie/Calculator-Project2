0edab4669da6e2c40045e07a24bee378033fc586
DylanFyvie <dfyvie@hotmail.com>
2016-10-25 10:32:12 -0500

    Calculator project
    Mr.Hardman

---
+++ Calculator Project/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Calculator_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGame1 = new System.Windows.Forms.Label();
+            this.lblGame2 = new System.Windows.Forms.Label();
+            this.lblGame3 = new System.Windows.Forms.Label();
+            this.lblGame4 = new System.Windows.Forms.Label();
+            this.lblGame5 = new System.Windows.Forms.Label();
+            this.lblGame6 = new System.Windows.Forms.Label();
+            this.lblGame7 = new System.Windows.Forms.Label();
+            this.lblGame8 = new System.Windows.Forms.Label();
+            this.lblGame9 = new System.Windows.Forms.Label();
+            this.lblGame10 = new System.Windows.Forms.Label();
+            this.txbGame1 = new System.Windows.Forms.TextBox();
+            this.txbGame2 = new System.Windows.Forms.TextBox();
+            this.txbGame3 = new System.Windows.Forms.TextBox();
+            this.txbGame4 = new System.Windows.Forms.TextBox();
+            this.txbGame5 = new System.Windows.Forms.TextBox();
+            this.txbGame6 = new System.Windows.Forms.TextBox();
+            this.txbGame7 = new System.Windows.Forms.TextBox();
+            this.txbGame8 = new System.Windows.Forms.TextBox();
+            this.txbGame9 = new System.Windows.Forms.TextBox();
+            this.txbGame10 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblGame1
+            // 
+            this.lblGame1.AutoSize = true;
+            this.lblGame1.Location = new System.Drawing.Point(28, 15);
+            this.lblGame1.Name = "lblGame1";
+            this.lblGame1.Size = new System.Drawing.Size(44, 13);
+            this.lblGame1.TabIndex = 0;
+            this.lblGame1.Text = "Game 1";
+            // 
+            // lblGame2
+            // 
+            this.lblGame2.AutoSize = true;
+            this.lblGame2.Location = new System.Drawing.Point(28, 40);
+            this.lblGame2.Name = "lblGame2";
+            this.lblGame2.Size = new System.Drawing.Size(44, 13);
+            this.lblGame2.TabIndex = 1;
+            this.lblGame2.Text = "Game 2";
+            // 
+            // lblGame3
+            // 
+            this.lblGame3.AutoSize = true;
+            this.lblGame3.Location = new System.Drawing.Point(28, 65);
+            this.lblGame3.Name = "lblGame3";
+            this.lblGame3.Size = new System.Drawing.Size(44, 13);
+            this.lblGame3.TabIndex = 2;
+            this.lblGame3.Text = "Game 3";
+            // 
+            // lblGame4
+            // 
+            this.lblGame4.AutoSize = true;
+            this.lblGame4.Location = new System.Drawing.Point(28, 90);
+            this.lblGame4.Name = "lblGame4";
+            this.lblGame4.Size = new System.Drawing.Size(44, 13);
+            this.lblGame4.TabIndex = 3;
+            this.lblGame4.Text = "Game 4";
+            this.lblGame4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // lblGame5
+            // 
+            this.lblGame5.AutoSize = true;
+            this.lblGame5.Location = new System.Drawing.Point(28, 115);
+            this.lblGame5.Name = "lblGame5";
+            this.lblGame5.Size = new System.Drawing.Size(44, 13);
+            this.lblGame5.TabIndex = 4;
+            this.lblGame5.Text = "Game 5";
+            // 
+            // lblGame6
+            // 
+            this.lblGame6.AutoSize = true;
+            this.lblGame6.Location = new System.Drawing.Point(28, 140);
+            this.lblGame6.Name = "lblGame6";
+            this.lblGame6.Size = new System.Drawing.Size(44, 13);
+            this.lblGame6.TabIndex = 5;
+            this.lblGame6.Text = "Game 6";
+            // 
+            // lblGame7
+            // 
+            this.lblGame7.AutoSize = true;
+            this.lblGame7.Location = new System.Drawing.Point(28, 165);
+            this.lblGame7.Name = "lblGame7";
+            this.lblGame7.Size = new System.Drawing.Size(44, 13);
+            this.lblGame7.TabIndex = 6;
+            this.lblGame7.Text = "Game 7";
+            this.lblGame7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // lblGame8
+            // 
+            this.lblGame8.AutoSize = true;
+            this.lblGame8.Location = new System.Drawing.Point(28, 190);
+            this.lblGame8.Name = "lblGame8";
+            this.lblGame8.Size = new System.Drawing.Size(44, 13);
+            this.lblGame8.TabIndex = 7;
+            this.lblGame8.Text = "Game 8";
+            // 
+            // lblGame9
+            // 
+            this.lblGame9.AutoSize = true;
+            this.lblGame9.Location = new System.Drawing.Point(28, 215);
+            this.lblGame9.Name = "lblGame9";
+            this.lblGame9.Size = new System.Drawing.Size(44, 13);
+            this.lblGame9.TabIndex = 8;
+            this.lblGame9.Text = "Game 9";
+            // 
+            // lblGame10
+            // 
+            this.lblGame10.AutoSize = true;
+            this.lblGame10.Location = new System.Drawing.Point(28, 240);
+            this.lblGame10.Name = "lblGame10";
+            this.lblGame10.Size = new System.Drawing.Size(50, 13);
+            this.lblGame10.TabIndex = 9;
+            this.lblGame10.Text = "Game 10";
+            // 
+            // txbGame1
+            // 
+            this.txbGame1.Location = new System.Drawing.Point(113, 15);
+            this.txbGame1.Name = "txbGame1";
+            this.txbGame1.Size = new System.Drawing.Size(100, 20);
+            this.txbGame1.TabIndex = 10;
+            // 
+            // txbGame2
+            // 
+            this.txbGame2.Location = new System.Drawing.Point(113, 40);
+            this.txbGame2.Name = "txbGame2";
+            this.txbGame2.Size = new System.Drawing.Size(100, 20);
+            this.txbGame2.TabIndex = 11;
+            // 
+            // txbGame3
+            // 
+            this.txbGame3.Location = new System.Drawing.Point(113, 65);
+            this.txbGame3.Name = "txbGame3";
+            this.txbGame3.Size = new System.Drawing.Size(100, 20);
+            this.txbGame3.TabIndex = 12;
+            // 
+            // txbGame4
+            // 
+            this.txbGame4.Location = new System.Drawing.Point(113, 90);
+            this.txbGame4.Name = "txbGame4";
+            this.txbGame4.Size = new System.Drawing.Size(100, 20);
+            this.txbGame4.TabIndex = 13;
+            // 
+            // txbGame5
+            // 
+            this.txbGame5.Location = new System.Drawing.Point(113, 115);
+            this.txbGame5.Name = "txbGame5";
+            this.txbGame5.Size = new System.Drawing.Size(100, 20);
+            this.txbGame5.TabIndex = 14;
+            // 
+            // txbGame6
+            // 
+            this.txbGame6.Location = new System.Drawing.Point(113, 142);
+            this.txbGame6.Name = "txbGame6";
+            this.txbGame6.Size = new System.Drawing.Size(100, 20);
+            this.txbGame6.TabIndex = 15;
+            // 
+            // txbGame7
+            // 
+            this.txbGame7.Location = new System.Drawing.Point(113, 165);
+            this.txbGame7.Name = "txbGame7";
+            this.txbGame7.Size = new System.Drawing.Size(100, 20);
+            this.txbGame7.TabIndex = 16;
+            // 
+            // txbGame8
+            // 
+            this.txbGame8.Location = new System.Drawing.Point(113, 190);
+            this.txbGame8.Name = "txbGame8";
+            this.txbGame8.Size = new System.Drawing.Size(100, 20);
+            this.txbGame8.TabIndex = 17;
+            // 
+            // txbGame9
+            // 
+            this.txbGame9.Location = new System.Drawing.Point(113, 215);
+            this.txbGame9.Name = "txbGame9";
+            this.txbGame9.Size = new System.Drawing.Size(100, 20);
+            this.txbGame9.TabIndex = 18;
+            // 
+            // txbGame10
+            // 
+            this.txbGame10.Location = new System.Drawing.Point(113, 240);
+            this.txbGame10.Name = "txbGame10";
+            this.txbGame10.Size = new System.Drawing.Size(100, 20);
+            this.txbGame10.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 355);
+            this.Controls.Add(this.txbGame10);
+            this.Controls.Add(this.txbGame9);
+            this.Controls.Add(this.txbGame8);
+            this.Controls.Add(this.txbGame7);
+            this.Controls.Add(this.txbGame6);
+            this.Controls.Add(this.txbGame5);
+            this.Controls.Add(this.txbGame4);
+            this.Controls.Add(this.txbGame3);
+            this.Controls.Add(this.txbGame2);
+            this.Controls.Add(this.txbGame1);
+            this.Controls.Add(this.lblGame10);
+            this.Controls.Add(this.lblGame9);
+            this.Controls.Add(this.lblGame8);
+            this.Controls.Add(this.lblGame7);
+            this.Controls.Add(this.lblGame6);
+            this.Controls.Add(this.lblGame5);
+            this.Controls.Add(this.lblGame4);
+            this.Controls.Add(this.lblGame3);
+            this.Controls.Add(this.lblGame2);
+            this.Controls.Add(this.lblGame1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGame1;
+        private System.Windows.Forms.Label lblGame2;
+        private System.Windows.Forms.Label lblGame3;
+        private System.Windows.Forms.Label lblGame4;
+        private System.Windows.Forms.Label lblGame5;
+        private System.Windows.Forms.Label lblGame6;
+        private System.Windows.Forms.Label lblGame7;
+        private System.Windows.Forms.Label lblGame8;
+        private System.Windows.Forms.Label lblGame9;
+        private System.Windows.Forms.Label lblGame10;
+        private System.Windows.Forms.TextBox txbGame1;
+        private System.Windows.Forms.TextBox txbGame2;
+        private System.Windows.Forms.TextBox txbGame3;
+        private System.Windows.Forms.TextBox txbGame4;
+        private System.Windows.Forms.TextBox txbGame5;
+        private System.Windows.Forms.TextBox txbGame6;
+        private System.Windows.Forms.TextBox txbGame7;
+        private System.Windows.Forms.TextBox txbGame8;
+        private System.Windows.Forms.TextBox txbGame9;
+        private System.Windows.Forms.TextBox txbGame10;
+    }
+}
+
